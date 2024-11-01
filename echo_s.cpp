a54633cb7f5ba76d3e62e41deaa25bf187c82725
Nacho02s <74149764+Nacho02s@users.noreply.github.com>
2024-11-01 11:42:17 -0600

    Update echo_s.cpp

--- echo_s.cpp
+++ echo_s.cpp
@@ -2,8 +2,15 @@
 // * Echo Strings (echo_s.cc)
 // * -- Accepts TCP connections and then echos back each string sent.
 // **************************************************************************************
+#include <iostream>
+#include <cstring>
+#include <unistd.h>
+#include <arpa/inet.h>
 #include "echo_s.h"
+#include "logging.h"  
 
+#define DEFAULT_PORT 12345
+#define BUFFER_SIZE 1024
 
 // **************************************************************************************
 // * processConnection()
@@ -11,29 +18,34 @@
 // * - Returns true if the client sends "QUIT" command, false if the client sends "CLOSE".
 // **************************************************************************************
 int processConnection(int sockFd) {
+    bool quitProgram = false;
+    bool keepGoing = true;
 
-  bool quitProgram = true;
-  bool keepGoing = true;
-  while (keepGoing) {
+    while (keepGoing) {
+        char buffer[BUFFER_SIZE];
+        memset(buffer, 0, sizeof(buffer));
 
-    //
-    // Call read() call to get a buffer/line from the client.
-    // Hint - don't forget to zero out the buffer each time you use it.
-    //
+        // Call read() to get a buffer/line from the client.
+        int bytesRead = read(sockFd, buffer, sizeof(buffer) - 1);
+        if (bytesRead < 0) {
+            perror("Read error");
+            return false;
+        }
 
-    //
-    // Check for one of the commands
-    //
+        // Check for one of the commands
+        if (strncmp(buffer, "QUIT", 4) == 0) {
+            quitProgram = true;
+            keepGoing = false;
+        } else if (strncmp(buffer, "CLOSE", 5) == 0) {
+            keepGoing = false;
+        } else {
+            // Call write() to send line back to the client.
+            write(sockFd, buffer, bytesRead);
+        }
+    }
 
-    //
-    // Call write() to send line back to the client.
-    //
-
-  }
-
-  return quitProgram;
+    return quitProgram;
 }
-    
 
 
 // **************************************************************************************
@@ -41,90 +53,86 @@ int processConnection(int sockFd) {
 // * - Sets up the sockets and accepts new connection until processConnection() returns 1
 // **************************************************************************************
 
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
 
-  // ********************************************************************
-  // * Process the command line arguments
-  // ********************************************************************
-  int opt = 0;
-  while ((opt = getopt(argc,argv,"d:")) != -1) {
-    
-    switch (opt) {
-    case 'd':
-      LOG_LEVEL = std::stoi(optarg);;
-      break;
-    case ':':
-    case '?':
-    default:
-      std::cout << "useage: " << argv[0] << " -d <num>" << std::endl;
-      exit(-1);
+    // ********************************************************************
+    // * Process the command line arguments
+    // ********************************************************************
+    int opt = 0;
+    while ((opt = getopt(argc, argv, "d:")) != -1) {
+        switch (opt) {
+            case 'd':
+                LOG_LEVEL = std::stoi(optarg);
+                break;
+            case ':':
+            case '?':
+            default:
+                std::cout << "Usage: " << argv[0] << " -d <num>" << std::endl;
+                exit(-1);
+        }
     }
-  }
 
-  // *******************************************************************
-  // * Creating the inital socket is the same as in a client.
-  // ********************************************************************
-  int     listenFd = -1;
-  // Call socket() to create the socket you will use for lisening.
+    // *******************************************************************
+    // * Creating the initial socket is the same as in a client.
+    // ********************************************************************
+    int listenFd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listenFd < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
 
-  
-  // ********************************************************************
-  // * The bind() and calls take a structure that specifies the
-  // * address to be used for the connection. On the cient it contains
-  // * the address of the server to connect to. On the server it specifies
-  // * which IP address and port to lisen for connections.
-  // ********************************************************************
-  struct sockaddr_in servaddr;
-  memset(&servaddr, 0, sizeof(servaddr));
+    // ********************************************************************
+    // * The bind() and calls take a structure that specifies the
+    // * address to be used for the connection. On the client it contains
+    // * the address of the server to connect to. On the server it specifies
+    // * which IP address and port to listen for connections.
+    // ********************************************************************
+    struct sockaddr_in servaddr;
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(DEFAULT_PORT);
 
-  // *** assign 3 fields in the servadd struct sin_family, sin_addr.s_addr and sin_port
-  // *** the value your port can be any value > 1024.
+    bool bindSuccessful = false;
+    while (!bindSuccessful) {
+        if (bind(listenFd, (struct sockaddr *)&servaddr, sizeof(servaddr)) == 0) {
+            bindSuccessful = true;
+        } else {
+            perror("Bind failed, retrying on a different port");
+            servaddr.sin_port = htons(ntohs(servaddr.sin_port) + 1);  // Increment port
+        }
+    }
 
+    printf("Using port: %d\n", ntohs(servaddr.sin_port));
 
-  // ********************************************************************
-  // * Binding configures the socket with the parameters we have
-  // * specified in the servaddr structure.  This step is implicit in
-  // * the connect() call, but must be explicitly listed for servers.
-  // ********************************************************************
-  bool bindSuccesful = false;
-  while (!bindSuccesful) {
-    // ** Call bind()
-    // You may have to call bind multiple times if another process is already using the port
-    // your program selects.
-  }
-  // *** DON'T FORGET TO PRINT OUT WHAT PORT YOUR SERVER PICKED SO YOU KNOW HOW TO CONNECT.
+    // ********************************************************************
+    // * Setting the socket to the listening state is the second step
+    // * needed to begin accepting connections. This creates a queue for
+    // * connections and starts the kernel listening for connections.
+    // ********************************************************************
+    int listenQueueLength = 1;
+    if (listen(listenFd, listenQueueLength) < 0) {
+        perror("Listen failed");
+        exit(EXIT_FAILURE);
+    }
 
+    // ********************************************************************
+    // * The accept call will sleep, waiting for a connection. When
+    // * a connection request comes in, the accept() call creates a NEW
+    // * socket with a new fd that will be used for the communication.
+    // ********************************************************************
+    bool quitProgram = false;
+    while (!quitProgram) {
+        int connFd = accept(listenFd, NULL, NULL);
+        if (connFd < 0) {
+            perror("Accept failed");
+            continue;
+        }
 
-  // ********************************************************************
-  // * Setting the socket to the listening state is the second step
-  // * needed to being accepting connections.  This creates a queue for
-  // * connections and starts the kernel listening for connections.
-  // ********************************************************************
-  int listenQueueLength = 1;
-  // ** Cal listen()
+        quitProgram = processConnection(connFd);
+        close(connFd);
+    }
 
-  // ********************************************************************
-  // * The accept call will sleep, waiting for a connection.  When 
-  // * a connection request comes in the accept() call creates a NEW
-  // * socket with a new fd that will be used for the communication.
-  // ********************************************************************
-  bool quitProgram = false;
-  while (!quitProgram) {
-    int connFd = 0;
-
-    // Call the accept() call to check the listening queue for connection requests.
-    // If a client has already tried to connect accept() will complete the
-    // connection and return a file descriptor that you can read from and
-    // write to. If there is no connection waiting accept() will block and
-    // not return until there is a connection.
-    // ** call accept() 
-    
-    // Now we have a connection, so you can call processConnection() to do the work.
-    quitProgram = processConnection(connFd);
-   
-    close(connFd);
-  }
-
-  close(listenFd);
-
+    close(listenFd);
+    return 0;
 }
